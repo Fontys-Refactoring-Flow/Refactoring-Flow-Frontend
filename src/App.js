e254ef7bdc4736aca8f27978e6d5e9cc1a237239
Rik de Goede <rik.dgd@gmail.com>
2022-03-22 12:35:25 +0100

    added first version of footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import AboutUs from './components/AboutUs';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AboutUs />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import '../style/Footer.css'
 
 class Footer extends Component {
-    constructor(props) {
-        super(props);
-    }
+
     state = {  }
     render() { 
         return ( 
-            <footer>
-                
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-dark">
+                <div className="col-md-4 d-flex align-items-center">
+                    <span className="text-muted">© 2022 Refactory Flow, Inc</span>
+                </div>
             </footer>
-         );
+        );
     }
 }
  
