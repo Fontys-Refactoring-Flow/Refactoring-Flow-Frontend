92c819b11b7fd0ffb2b5242166dc3c37a5deccc1
lucvd <lucvd@LAPTOP-7R52BL44>
2022-03-29 13:42:27 +0200

    :100:  Styling update

--- src/components/AssignmentComponents/AssignmentsPage.jsx
+++ src/components/AssignmentComponents/AssignmentsPage.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import AssignmentService from '../../Services/AssignmentService';
 import '../../style/AssignmentsPage.css'
 import '../../style/Button.css'
+import '../../style/Card.css'
 import { Link } from 'react-router-dom';
 
 
 class AssignmentsPage extends Component {
     constructor(props) {
         super(props);
+
 
         this.state = {
             challenge: []
@@ -21,18 +23,22 @@ class AssignmentsPage extends Component {
         })
     }
 
+
     render() {
         return (
             <div className='container' style={{ marginTop: '50px', display: 'flex' }}>
                 {
                     this.state.challenge.map(
                         challenge =>
-                            <div className="card" style={{ width: '18rem', margin: '10px' }}>
+                            <div className="card card-container" style={{ width: '18rem', margin: '10px' }}>
                                 <div className="card-body">
                                     <h5 className="card-title">{challenge.name}</h5>
                                     <p className="card-text">{challenge.description}</p>
-                                    <Link to='/assignment-details' className='btn button-card text-white' role="button">Show more info</Link>
                                 </div>
+                                <button className='btn btn-primary' type='button'>
+                                    Show more info
+                                </button>
+                                <Link to='/assignment-details' className='btn button-card text-white' role="button">Select challenge</Link>
                             </div>
                     )
                 }
