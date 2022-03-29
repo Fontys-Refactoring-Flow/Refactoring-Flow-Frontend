80c576c10de3469829aaa8c5158bb9b311994d84
lucvd <lucvd@LAPTOP-7R52BL44>
2022-03-29 15:16:42 +0200

    :100: Cards werken nu in assignment

--- src/components/AssignmentComponents/AssignmentsPage.jsx
+++ src/components/AssignmentComponents/AssignmentsPage.jsx
@@ -5,7 +5,6 @@ import '../../style/Button.css'
 import '../../style/Card.css'
 import { Link } from 'react-router-dom';
 
-
 class AssignmentsPage extends Component {
     constructor(props) {
         super(props);
@@ -26,23 +25,37 @@ class AssignmentsPage extends Component {
 
     render() {
         return (
-            <div className='container' style={{ marginTop: '50px', display: 'flex' }}>
+            <div>
+                <h1 >Challenges available</h1>
+                <div className="card-container">
                 {
                     this.state.challenge.map(
                         challenge =>
-                            <div className="card card-container" style={{ width: '18rem', margin: '10px' }}>
-                                <div className="card-body">
-                                    <h5 className="card-title">{challenge.name}</h5>
-                                    <p className="card-text">{challenge.description}</p>
+                            <tr key={challenge.id}>
+                                <div className='card' style={{ width: '18rem', height: '200px', margin: '10px' }}>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{challenge.name}</h5>
+                                        <p className="card-text">{challenge.description}</p>
+                                        <div class="collapse" id={"collapseExample" + challenge.id}>
+                                            <p class="card card-body">
+                                                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                                            </p>
+                                        </div>
+                                        <div className='btn-holder'>
+                                            <button class="btn btn-refactoring text-white" type="button" data-bs-toggle="collapse" data-bs-target={"#collapseExample" + challenge.id} aria-expanded="false" aria-controls="collapseExample">
+                                                Show more info
+                                            </button>
+                                            <Link to='/assignment-details' className='btn btn-refactoring text-white' role="button">Start challenge</Link>
+                                        </div>
+                                    </div>
                                 </div>
-                                <button className='btn btn-primary' type='button'>
-                                    Show more info
-                                </button>
-                                <Link to='/assignment-details' className='btn button-card text-white' role="button">Select challenge</Link>
-                            </div>
+                            </tr>
                     )
                 }
             </div>
+            </div>
+            
+            
         );
     }
 }
