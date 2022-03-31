de9087ca2a4cbba781bacebbc913838a92ad2c75
TheRealNock <92985337+TheRealNock@users.noreply.github.com>
2022-03-31 09:41:45 +0200

    :sparkles: ProgressPage

--- src/components/ProgressPage.jsx
+++ src/components/ProgressPage.jsx
@@ -11,7 +11,7 @@ const ProgressPage = () => {
             <div class="card-body">
             <h5 class="card-title">Assignment name</h5>
             <p class="card-text">Uitleg Assignment</p>
-            <a href="#" class="card-button">Details</a>
+            <a href="#" class="card-button">Assignment Details</a>
             </div>
         </div>
         <p className='title2'>Reviewed assignments:</p>
@@ -19,7 +19,7 @@ const ProgressPage = () => {
             <div class="card-body">
             <h5 class="card-title">Assignment name</h5>
             <p class="card-text">Uitleg Assignment</p>
-            <Link to='/about'  class="card-button">Details</Link> 
+            <Link to='/about'  class="card-button">See feedback </Link> 
             </div>
         </div>
     </div>
