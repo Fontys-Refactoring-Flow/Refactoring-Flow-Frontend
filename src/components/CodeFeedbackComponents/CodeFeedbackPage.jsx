0470f23a292e6b1239947f375e57038b38908251
Rik de Goede <rik.dgd@gmail.com>
2022-04-07 14:45:45 +0200

    Added codeblock to the project
    this npm package is easy to use, but the code needs to be a string.

--- src/components/CodeFeedbackComponents/CodeFeedbackPage.jsx
+++ src/components/CodeFeedbackComponents/CodeFeedbackPage.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import FeedbackBubble from './FeedbackBubble';
 import '../../style/CodeFeedback.css'
+import { CodeBlock, dracula } from 'react-code-blocks';
+
+let code = 'package mine_sweeper_game;\n\npublic class Cell {\n    boolean isMarked;\n    boolean containsMine;\n    boolean isRevealed;\n    int xPos;\n    int yPos;\n\n    public Cell(int XPos, int YPos, boolean isMine) {'
 
 const CodeFeedbackPage = () => {
     return(
         <div className='container code-feedback-Container'>
-            <textarea rows='30' className='form-control code-field'></textarea>
+            <div className='code-field'>
+                <CodeBlock
+                    text={code}
+                    language='java'
+                    showLineNumbers='true'
+                    theme={dracula}
+                    warpLines
+                />
+            </div>
 
             <div className='feedback-container'>
                 <FeedbackBubble text='feedback'/>
