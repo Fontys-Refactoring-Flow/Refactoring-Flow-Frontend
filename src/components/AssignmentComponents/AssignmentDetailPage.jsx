0124046573d4c3ac7600f4e53e495a0fbbb0e621
lucvd <lucvd@LAPTOP-7R52BL44>
2022-04-19 09:49:03 +0200

    :100:  geen idee wat ik precies verandert heb, is alweer een week geleden

--- src/components/AssignmentComponents/AssignmentDetailPage.jsx
+++ src/components/AssignmentComponents/AssignmentDetailPage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AssignmentService from '../../Services/AssignmentService';
+import ProgressBar from "bootstrap-progress-bar";
 
 class AssignmentDetailPage extends Component {
     constructor(props) {
@@ -7,7 +8,9 @@ class AssignmentDetailPage extends Component {
 
         this.state = {
             challenge: [],
-            challengeid: 2
+            challengeid: 2,
+            codeQualityProgress: []
+            
         }
     }
 
@@ -25,19 +28,39 @@ class AssignmentDetailPage extends Component {
                 <table className='table table-striped table-bordered'>
                     <thead>
                         <tr>
-                            <th colspan="3" class="fwfoD_bGBk fwfoD_fsuY fwfoD_EMjX" scope="col" aria-sort="none">Learning outcomes</th>
+                            <th colspan="39" class="fwfoD_bGBk fwfoD_fsuY fwfoD_EMjX" scope="col" aria-sort="none">Learning outcomes</th>
                         </tr>
                         <tr>
-                            <th scope='col'>
+                            <th colspan="3" class="fwfoD_bGBk fwfoD_fsuY fwfoD_EMjX" scope="col" aria-sort="none" style={{ width: '20%' }}>Criteria</th>
+                            <th colspan="3" class="fwfoD_bGBk fwfoD_fsuY fwfoD_EMjX" scope="col" aria-sort="none">Ratings</th>
+                            <th colspan="3" class="fwfoD_bGBk fwfoD_fsuY fwfoD_EMjX" scope="col" aria-sort="none" style={{ width: '20%' }}>Points</th>
+                        </tr>
+                        <tr>
+                            <th colspan="3" class="fwfoD_bGBk fwfoD_fsuY fwfoD_EMjX" scope="col" aria-sort="none" style={{ width: '20%' }}>
                                 Code Quality
                             </th>
+                            <th colspan="3" class="fwfoD_bGBk fwfoD_fsuY fwfoD_EMjX" scope="col" aria-sort="none">
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{ width: "50%", backgroundColor: "#663366" }}></div>
+                                </div>
+                            </th>
+                            <th colspan="3" class="fwfoD_bGBk fwfoD_fsuY fwfoD_EMjX" scope="col" aria-sort="none" style={{ width: '20%' }}>
+
+                            </th>
+
                         </tr>
                         <tr>
                             <th scope='col'>
                                 Refactoring
                             </th>
+                            <th colspan="3" class="fwfoD_bGBk fwfoD_fsuY fwfoD_EMjX" scope="col" aria-sort="none">
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{ width: "75%", backgroundColor: "#e5007d" }}></div>
+                                </div>
+                            </th>
                         </tr>
                     </thead>
+
                 </table>
             </div>
         );
