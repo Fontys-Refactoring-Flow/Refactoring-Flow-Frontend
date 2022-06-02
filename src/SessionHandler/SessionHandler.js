54547587c27c27cf85eb1fb5d06271f9cd4f52d1
lucvd <lucvd@LAPTOP-7R52BL44>
2022-06-02 09:42:48 +0200

    :100:  login updates

--- src/SessionHandler/SessionHandler.js
+++ src/SessionHandler/SessionHandler.js
@@ -11,6 +11,19 @@ class SessionHandler {
     clearStudentId(){
         return sessionStorage.clear("studentid");
     }
+
+    clearLoginAttemps(){
+        return sessionStorage.setItem("loginAttempts", 0);
+    }
+
+    setLoginAttemps(){
+        let count = this.getLoginAttempts() + 1;
+        return sessionStorage.setItem("loginAttempts", count)
+    }
+
+    getLoginAttempts(){
+        return sessionStorage.getItem("loginAttemps");
+    }
 }
 
 export default new SessionHandler;
--- src/components/LoginComponents/Login.jsx
+++ src/components/LoginComponents/Login.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import LoginImage from "../../Images/LoginImage.png"
 import '../../style/AssignmentsPage.css'
 import '../../style/Button.css'
@@ -7,64 +7,71 @@ import '../../style/Image.css'
 import StudentService from '../../Services/StudentService'
 import SessionHandler from '../../SessionHandler/SessionHandler'
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
+function Login() {
 
-        this.state = {
-            email: "",
-            password: "",
-            studentid: 0
-        }
-        this.changeEmailHandler = this.changeEmailHandler.bind(this);
-        this.changePasswordHandler = this.changePasswordHandler.bind(this);
-    }
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [route, setRoute] = useState();
+    const [errorMessage, setErrorMessage] = useState();
 
-    componentDidMount(){
+    useEffect(() => {
         SessionHandler.clearStudentId();
-    }
+        if(SessionHandler.getLoginAttempts >= 1){
+            setErrorMessage("Wrong E-mail or Password, please try again")
+        }
+        else{
+            setErrorMessage("");
+        }
+    })
 
-    changeEmailHandler = (event) => {
+    function changeEmailHandler(event){
         event.preventDefault();
-        this.setState({ email: event.target.value });
+        setEmail(event.target.value);
     }
 
-    changePasswordHandler = (event) => {
+    function changePasswordHandler(event){
         event.preventDefault();
-        this.setState({ password: event.target.value });
+        setPassword(event.target.value);
     }
 
-    handleLogin = () => {
-        StudentService.getLogin(this.state.email, this.state.password).then((res) => {
-            this.setState({ studentid: res.data.id })
-            console.log(this.state.studentid);
-            SessionHandler.setStudentId(this.state.studentid);
+    function handleLogin(){
+        StudentService.getLogin(email, password).then((res) => {
+            console.log(res.data.id);
+            if(res.data.id === undefined){
+                SessionHandler.setLoginAttemps();
+                //setRoute("/login");
+            }
+            else
+            {
+                SessionHandler.setStudentId(res.data.id);
+                SessionHandler.clearLoginAttemps();
+                //setRoute("/")
+            }
         })
     }
 
-    render() {
-        return (
-            <div className='d-flex align-items-start'>
-                <div  className='col-8' style={{'marginRight': '2%'}}>
-                    <img src={LoginImage} alt="fontys image" style={{'width': '100%', 'height': '100%vw'}}/>
-                </div>
-                <div className='col-3' style={{'marginLeft': '3%', 'marginRight': '2%', 'marginTop': '5%'}}>
-                    <form>
-                        <div className='form-group'>
-                            <h5>Login</h5>
-                            <label>E-mail address</label>
-                            <input placeholder='E-mail address' name='email' className='form-control' value={this.state.email} onChange={this.changeEmailHandler} />
-                            <label>Password</label>
-                            <input type="password" placeholder='Password' name='password' className='form-control' value={this.state.password} onChange={this.changePasswordHandler} />
-                            <a className="btn btn-refactoring text-white" onClick={this.handleLogin} href='/' style={{ marginTop: '10px' }}>
-                                Login
-                            </a>
-                        </div>
-                    </form>
-                </div>
+    return (
+        <div className='d-flex align-items-start'>
+            <div className='col-8' style={{ 'marginRight': '2%' }}>
+                <img src={LoginImage} alt="fontys image" style={{ 'width': '100%', 'height': '100%vw' }} />
             </div>
-        );
-    }
+            <div className='col-3' style={{ 'marginLeft': '3%', 'marginRight': '2%', 'marginTop': '5%' }}>
+                <form>
+                    <div className='form-group'>
+                        <h5>Login</h5>
+                        <h8>{errorMessage}</h8>
+                        <label>E-mail address</label>
+                        <input placeholder='E-mail address' name='email' className='form-control' value={email} onChange={changeEmailHandler} />
+                        <label>Password</label>
+                        <input type="password" placeholder='Password' name='password' className='form-control' value={password} onChange={changePasswordHandler} />
+                        <a className="btn btn-refactoring text-white" onClick={handleLogin}  style={{ marginTop: '10px' }}>
+                            Login
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
