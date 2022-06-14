1aa8c8a36a5b908ef37f61d790dd3bae1d8ceeb6
Rik de Goede <rik.dgd@gmail.com>
2022-06-14 10:26:05 +0200

    Added risks to demo

--- src/components/AssignmentComponents/AssignmentWorkspace.jsx
+++ src/components/AssignmentComponents/AssignmentWorkspace.jsx
@@ -3,6 +3,7 @@ import CodeField from '../CodeFeedbackComponents/Codefield';
 import CodeService from '../../Services/CodeService';
 import { useParams } from 'react-router-dom';
 import '../../style/AssignmentWorkspace.css';
+import StepAccordion from './StepAccordion';
 
 const AssignmentWorkspace = () => {
 
@@ -24,38 +25,7 @@ const AssignmentWorkspace = () => {
             <div className='editor-container code-field'>
                 <CodeField code={code}/>
             </div>
-            <div className='steps-container'>
-                <div class="accordion step-acordion" id="accordionExample">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingOne">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                            Reveal Solution
-                        </button>
-                        </h2>
-                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                <p><strong>Create new method named <i>isfull</i></strong></p>
-                                <p>
-                                    Create a new method that looks at the animals list to see if it contains to much animals.
-                                    In this example the limit is set to <i>10</i>.
-                                </p>
-                                <br/>
-                                <p><strong>Remove if statement from <i>addAnimal</i> method.</strong></p>
-                                <p>
-                                    Remove the if statement on lines 7-9 and add it to the new method, seperating the if statement
-                                    from the AddAnimal method
-                                </p>
-                                <br/>
-                                <p><strong>Use new method in old method</strong></p>
-                                <p>
-                                    The AddAnimal method still needs to perform the check for available space, 
-                                    so the new method should be used on the location of the old if statement. 
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <StepAccordion/>
         </div>
      );
 }
