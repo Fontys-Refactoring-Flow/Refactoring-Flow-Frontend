ff6388ebf773952143926fbe562ec492412f3074
IemandDieAbeHeet <abe.vriens@gmail.com>
2022-10-13 15:05:25 +0200

    Change service export to use individual functions instead of complete class

--- src/services/learningOutcomes.service.ts
+++ src/services/learningOutcomes.service.ts
@@ -2,11 +2,8 @@ import axios from "axios";
 
 const LEARNING_OUTCOMES_API_BASE_URL = "http://localhost:8080/api/v1/learning_outcomes";
 
-class LearningOutcomesService{
-    
-    getLearningOutcomesByStudentId(studentId: string){
-        return axios.get(LEARNING_OUTCOMES_API_BASE_URL + "/studentid/" + studentId)
-    }
+const getLearningOutcomesByStudentId = (studentId: string) => {
+    return axios.get(LEARNING_OUTCOMES_API_BASE_URL + "/studentid/" + studentId)
 }
 
-export default new LearningOutcomesService()
+export default { getLearningOutcomesByStudentId }
