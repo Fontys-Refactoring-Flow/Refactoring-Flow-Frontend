d4104a868db34f6d21f5ca209aa6754313645407
IemandDieAbeHeet <abe.vriens@gmail.com>
2022-10-19 09:50:46 +0200

    Remove unused imports

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import '../../style/Header.css'
 import { Link } from 'react-router-dom'
 import {useAuth} from "../context/AuthContext";
